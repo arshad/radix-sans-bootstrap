ef3553066a02e402b5dbbd2d03fd504f25f1a6cc
Arshad Chummun <arshad@chummun.com>
2014-05-22 15:01:55 +0400

    Removed all bootstrap js

--- assets/javascripts/radix-script.js
+++ assets/javascripts/radix-script.js
@@ -3,16 +3,4 @@
  * JS for Radix.
  */
 (function ($, Drupal, window, document, undefined) {
-  $(document).ready(function() {
-    // Show tab if hash is provided via url.
-    if (hash = window.location.hash) {
-      if ($('[href="' + hash + '"]').length) {
-        $('[href="' + hash + '"]').tab('show');
-      }
-    }
-    else {
-      // Show the first tab.
-      $('[data-toggle="tab"]').first().tab('show');
-    }
-  });
 })(jQuery, Drupal, this, this.document);
